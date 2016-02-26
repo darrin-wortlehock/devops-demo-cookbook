0e5fe7ca8441f4cc2b6fd4592d08538499528121
Darrin Wortlehock <darrin@wortlehock.net>
2016-02-26 16:18:00 +0000

    create chef config for go-agent user

--- recipes/continuous_delivery.rb
+++ recipes/continuous_delivery.rb
@@ -17,6 +17,20 @@ include_recipe 'golang::default'
 
 include_recipe 'gocd::server'
 
+template '/var/go/.chef/config.rb' do
+  source 'chef-config.rb.erb'
+  variables(
+      chef_server_url: node['gocd']['agent']['chef']['server_url'],
+      node_name: node['gocd']['agent']['chef']['node_name'],
+      client_key: node['gocd']['agent']['chef']['client_key']
+  )
+end
+
+awscli_s3_file node['gocd']['agent']['chef']['client_key'] do
+  bucket data_bag_item('terraform','aws_s3_bucket.devops-demo-secrets')['name']
+  key node['gocd']['agent']['chef']['node_name']
+end
+
 template '/etc/go/cruise-config.xml' do
   source 'cruise-config.xml.erb'
   variables(
