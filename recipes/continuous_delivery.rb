e44a5cf65179d4514cc1b5346cdde829c6ea535c
Darrin Wortlehock <darrin@wortlehock.net>
2016-02-29 12:30:08 +0000

    code style

--- recipes/continuous_delivery.rb
+++ recipes/continuous_delivery.rb
@@ -44,7 +44,11 @@ template '/var/go/.berkshelf/config.json' do
   source 'berks-config.json.erb'
 end
 
-secrets_bucket = data_bag_item('terraform', 'aws_s3_bucket.devops-demo-secrets')['bucket']
+secrets_bucket = data_bag_item(
+  'terraform',
+  'aws_s3_bucket.devops-demo-secrets'
+)['bucket']
+
 Chef::Log.warn("Using Secrets Bucket: #{secrets_bucket}")
 
 log 'secrets_bucket' do
